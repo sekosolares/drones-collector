2987e2ccafb2ec0489af02d652d74559539dad91
sekosolares <axel.solares.g@gmail.com>
2023-02-24 02:59:38 -0600

    [UPDATE] Title of main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  let username = 'Seko_Solares';
+</script>
+
+<h1> Welcome to Drone Collector Page </h1>
+<h2> My name is {username} </h2>
