ed91c9054a7c573484cb8ef0343f07ab72679dcc
sekosolares <axel.solares.g@gmail.com>
2023-02-24 03:00:00 -0600

    [ADD] Drones route to list the drones

---
+++ src/routes/drones/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  let listOfDrones = [
+    'Drone 1',
+    'Heliway 901 HS'
+  ]
+</script>
+
+<h3> List of Drones: </h3>
+
+<ul>
+  {#each listOfDrones as drone}
+    <li>{drone}</li>
+  {/each}
+</ul>
