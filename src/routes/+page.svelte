8bbd8ed75806e3943524b456004b1d9479cf5b63
sekosolares <axel.solares.g@gmail.com>
2023-03-04 01:58:40 -0600

    [ADD] Very basic route navigation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,3 +4,4 @@
 
 <h1> Welcome to Drone Collector Page </h1>
 <h2> My name is {username} </h2>
+<div>Check out my drones collection <a href="/drones">here</a>!</div>
--- src/routes/drones/+page.svelte
+++ src/routes/drones/+page.svelte
@@ -3,10 +3,20 @@
     'Drone 1',
     'Heliway 901 HS'
   ]
+  function addDrone() {
+    const newDroneName = document.getElementById("NewDrone");
+    listOfDrones = [...listOfDrones, newDroneName.value];
+    newDroneName.value = '';
+  }
 </script>
+
+<a href="/"> Home </a>
 
 <h3> List of Drones: </h3>
 
+
+<input type="text" name="newDrone" id="NewDrone">
+<button on:click={addDrone}>Add Drone</button>
 <ul>
   {#each listOfDrones as drone}
     <li>{drone}</li>
